3dcf2805b7b3a1709fdf8f877949b6942490a71b
John Devine <johnjdevine@yahoo.com>
2013-01-10 13:08:55 -0500

    whoops

--- app/overrides/admin_shared_order_tabs_print_buttons_decorator.rb
+++ app/overrides/admin_shared_order_tabs_print_buttons_decorator.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :name => "print_buttons",
                      :insert_bottom => "ul[data-hook='admin_order_tabs']",
                      :partial => "spree/admin/orders/print_buttons",
-                     :disabled => false
+                     :disabled => false)
